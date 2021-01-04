dbfb31d1f39fe696d03654497dcfff3dc0dbec63
mathiasare <are.mathias@gmail.com>
2021-01-04 15:56:20 +0200

    completed exam practical task

--- src/components/Entries.vue
+++ src/components/Entries.vue
@@ -1,12 +1,61 @@
 <template>
+<div >
+    <button  v-on:click="reverseEntries">{{buttonText}}</button>
+    <div class="entry" v-for="entry in entries" :key="entry.id">
+            <ul>
+                <li><h3>{{entry.title}}</h3></li>
+                <li>{{entry.date | formatDate}}</li>
+                <li><img  :src="entry.image" alt="post-image"></li>
+                <li>{{entry.text}}</li>
+            </ul>
+
+    </div>
+</div>
 </template>
 
 <script>
+import moment from 'moment'
     export default {
         name: 'Entries',
+        data: function () {
+            return {
+            buttonText: "Newest to oldest",
+            
+            }},
         props: {
             entries: Array
+
+            
+
+        },
+        filters: {
+             formatDate: function (value) {
+                return moment(value).format('LLLL');
+            }
+
+
+        },
+        computed: {
+            sortedEntries: function(){
+                let newArray = this.entries;
+                return newArray.sort((a, b) => (new Date(a.date) < new Date(b.date)) ? 1 : -1);
+            }
+        },
+        methods:{
+            reverseEntries: function (){
+                let newArray = this.entries;
+                if(this.buttonText=="Newest to oldest"){
+                    this.buttonText="Oldest to newest";
+                    this.entries=newArray.sort((a, b) => (new Date(a.date) < new Date(b.date)) ? 1 : -1)
+                }else{
+                    this.buttonText="Newest to oldest"
+                    this.entries=newArray.sort((a, b) => (new Date(a.date) > new Date(b.date)) ? 1 : -1)
+                }
+                
+                
+            }
         }
+
     }
 </script>
 
@@ -19,10 +68,12 @@
     ul {
         list-style-type: none;
         padding: 0;
+        display: flex;
+        flex-direction: column;
     }
 
     li {
-        display: inline-block;
+        
         margin: 0 10px;
     }
 
